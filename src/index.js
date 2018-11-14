0b0122467ecda9635d2b49d2d9cbba55e5ae6b04
Eve Mendelevich <ym9382@gmail.com>
2018-11-13 22:03:52 -0500

    added styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const mapboxgl = require('mapbox-gl');
-const createElement = require('./marker');
+const addMarker = require('./marker');
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZXZlbWVuZGVsZXZpY2giLCJhIjoiY2pvZzlzc3Y5MDExdzN2cXQzYnZlZWI3OCJ9.L6N6O-Nb278QF551ERH10g';
@@ -11,6 +11,6 @@ const map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
 });
 
-// createAll().forEach(elem => elem.addTo(map));
-
-createElement().addTo(map);
+addMarker('activity', [-74.009, 40.705], map);
+addMarker('hotel', [-74.015, 40.705], map);
+addMarker('restaurant', [-74.009, 40.71], map);
--- src/marker.js
+++ src/marker.js
@@ -1,26 +1,17 @@
 const mapboxgl = require('mapbox-gl');
-// const markersArr = [
-//   { url: 'url(http://i.imgur.com/WbMOfMl.png)', coor: [-74.009, 40.705] },
-//   //   { url: 'url(http://i.imgur.com/WbMOfMl.png)', coor: [-87.6354, 41.8885] },
-// ];
 
-function createElement() {
-  console.log('I am being run!');
+const iconURLs = {
+  hotel: 'http://i.imgur.com/D9574Cu.png',
+  restaurant: 'http://i.imgur.com/cqR6pUI.png',
+  activity: 'http://i.imgur.com/WbMOfMl.png',
+};
+
+function addMarker(type, coor, map) {
   const markerDomEl = document.createElement('div');
   markerDomEl.style.width = '32px';
   markerDomEl.style.height = '39px';
-  markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
-  return new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]);
+  markerDomEl.style.backgroundImage = `url(${iconURLs[type]})`;
+  new mapboxgl.Marker(markerDomEl).setLngLat(coor).addTo(map);
 }
 
-// function createAll() {
-//   const arr = markersArr.map(element =>
-//     createElement(element.url, element.coor)
-//   );
-
-//   console.log(arr);
-
-//   return arr;
-// }
-
-module.exports = createElement;
+module.exports = addMarker;
